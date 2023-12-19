659f784187440235acd314d37093c603c7dbe418
Kelian <48365713+Hartox06@users.noreply.github.com>
2023-12-19 13:23:30 -0400

    Update FastestPath.java

--- FastestPath.java
+++ FastestPath.java
@@ -1,6 +1,7 @@
 package finalproject;
 
 import finalproject.system.Tile;
+import finalproject.system.TileType;
 import finalproject.tiles.MetroTile;
 
 public class FastestPath extends PathFindingService {
@@ -8,16 +9,18 @@ public class FastestPath extends PathFindingService {
     public FastestPath(Tile start) {
         super(start);
         generateGraph();
+
     }
 
-    @Override
-    public void generateGraph() {
+	@Override
+	public void generateGraph() {
+		// TODO Auto-generated method stub
         super.g = new Graph(GraphTraversal.BFS(source));
         for (Tile tile : GraphTraversal.BFS(source)) {
             for (Tile neighbour : tile.neighbors) {
-                if (neighbour.isWalkable()) {       //ADDED FOR TESTER
+                if (neighbour.isWalkable()) {
                     double weight = neighbour.timeCost;
-                    if (g.areMetros(tile,neighbour)) {      //ADDED FOR TESTER
+                    if (tile.type==TileType.Metro && neighbour.type==TileType.Metro) {
                         ((MetroTile) tile).fixMetro(neighbour);
                         weight = ((MetroTile) tile).metroTimeCost;
                     }
@@ -25,5 +28,6 @@ public class FastestPath extends PathFindingService {
                 }
             }
         }
-    }
+	}
+
 }
