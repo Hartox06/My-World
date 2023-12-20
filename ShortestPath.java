921f2815ed44690c7604f669be473c9f8461113f
Kelian <48365713+Hartox06@users.noreply.github.com>
2023-12-20 15:36:30 -0400

    Update ShortestPath.java

--- ShortestPath.java
+++ ShortestPath.java
@@ -2,8 +2,6 @@ package finalproject;
 
 
 import finalproject.system.Tile;
-import finalproject.system.TileType;
-import finalproject.tiles.MetroTile;
 
 public class ShortestPath extends PathFindingService {
     //TODO level 4: find distance prioritized path
@@ -16,15 +14,14 @@ public class ShortestPath extends PathFindingService {
 	public void generateGraph() {
 		// TODO Auto-generated method stub
         super.g = new Graph(GraphTraversal.BFS(source));
-        for (Tile tile : GraphTraversal.BFS(source)) {
-            for (Tile neighbour : tile.neighbors) {
-                if (neighbour.isWalkable()) {
-                    double weight = neighbour.distanceCost;
-                    if (tile.type == TileType.Metro && neighbour.type == TileType.Metro) {
-                        ((MetroTile) tile).fixMetro(neighbour);
-                        weight = ((MetroTile) tile).metroDistanceCost;
+        for (Tile specificTile : GraphTraversal.BFS(source)) {
+            for (Tile neighbor : specificTile.neighbors) {
+                if (neighbor.isWalkable()) {
+                    double totalWeight = neighbor.distanceCost;
+                    if (specificTile.isMetro() && neighbor.isMetro()) {
+                        totalWeight = g.helperLevel7Shortest(specificTile,neighbor);
                     }
-                    g.addEdge(tile, neighbour, weight);
+                    g.addEdge(specificTile, neighbor, totalWeight);
                 }
             }
         }
